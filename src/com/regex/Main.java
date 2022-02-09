9e21dbc2bcd79d5b203b265acf76fb9d08e45621
Ram11Coder <rambeliver11@gmail.com>
2022-02-09 23:55:25 +0530

    initial commit

---
+++ src/com/regex/Main.java
@@ -0,0 +1,13 @@
+package com.regex;
+
+/**
+ * Objective of POC is capture the all the possible regex to optimise our real
+ * time tedious work
+ *
+ *
+ */
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Welocome to pattern checker");
+	}
+}
